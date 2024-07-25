c50093eb3650828c8d3f69d8f84e9660aa0795a1
Simon Giraldo <simongiraldoc@gmail.com>
2024-07-24 23:55:52 -0500

    removed counter from excersi remove_element_in_place

--- c++/easy/remove_element_in_place.cpp
+++ c++/easy/remove_element_in_place.cpp
@@ -3,18 +3,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int notValCount = 0;
         int indexNotVal = 0;
 
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] != val) {
-                notValCount++;
                 nums[indexNotVal++] = nums[i];
             } else {
                 nums[i] = 0;
             }
         }
 
-        return notValCount;   
+        return indexNotVal;   
     }
 };
