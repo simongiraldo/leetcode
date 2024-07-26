48792c9c50b320669a0bb66124d9ff7b35a41656
Simon Giraldo <simongiraldoc@gmail.com>
2024-07-25 23:33:11 -0500

    remove duplicates in place, 2 pointers approach

---
+++ c++/easy/remove_duplicates_in_place.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int uniqueIndex = 1;
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[i - 1]){
+                nums[uniqueIndex] = nums[i];
+                uniqueIndex++;
+            }
+        }
+        
+        return uniqueIndex;
+    }
+};
