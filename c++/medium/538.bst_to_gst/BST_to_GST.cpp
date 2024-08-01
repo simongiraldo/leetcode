7469b21ffb1a85af5ba50c3dafcabcef67d164ac
Simon Giraldo <simongiraldoc@gmail.com>
2024-07-31 20:52:04 -0500

    exercise 538 added

---
+++ c++/medium/538.bst_to_gst/BST_to_GST.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if(root == NULL) return NULL;
+
+        greaterSum(root);
+
+        return root;
+    }
+    
+    TreeNode* greaterSum(TreeNode* root) {
+        if(root == NULL) return NULL;
+
+        TreeNode* rightSum = greaterSum(root->right);
+        if(rightSum) {
+            root->val += rightSum->val;
+        }
+
+        if(root->left) {
+            TreeNode* some = root->left;
+            while(some->right) {
+                some = some->right;
+            }
+            some->val += root->val;
+            
+            return greaterSum(root->left);
+        }
+
+        return root;
+    }
+
+
+};
