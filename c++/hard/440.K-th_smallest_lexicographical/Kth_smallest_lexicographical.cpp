939dd247f647d6ea10eea6faf31bbb1993ddf68d
Simon Giraldo <simongiraldoc@gmail.com>
2024-09-22 14:49:11 -0500

    second attempt, Kth elemetn lexiographic exercise

--- c++/hard/440.K-th_smallest_lexicographical/Kth_smallest_lexicographical.cpp
+++ c++/hard/440.K-th_smallest_lexicographical/Kth_smallest_lexicographical.cpp
@@ -4,14 +4,16 @@
 class Solution {
 public:
     int findKthNumber(int n, int k) {
-        vector<string> nums;
+        vector<int> nums;
 
         for(int i = 1; i <= n; i++) {
-            nums.push_back(to_string(i));
+            nums.push_back(i);
         }
 
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end(), [](int& a, int& b){
+            return to_string(a) < to_string(b);
+        });
 
-        return stoi(nums[k-1]);
+        return nums[k-1];
     }
 };
