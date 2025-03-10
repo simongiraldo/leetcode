7db389bdf47ce98d543601330044902a252d104c
Simon Giraldo <simongiraldoc@gmail.com>
2025-03-10 13:05:58 -0500

    exercise 3306 on c++, second version

--- c++/medium/3306.substrings_conatining_every_vowel_k_consonants_II/count_substring_with_every_vowel_and_k_consonants_II.cpp
+++ c++/medium/3306.substrings_conatining_every_vowel_k_consonants_II/count_substring_with_every_vowel_and_k_consonants_II.cpp
@@ -3,94 +3,69 @@ private:
     bool isVowel(char x) {
         return x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u';
     }
-
 public:
     long long countOfSubstrings(string word, int k) {
         if (word.size() < k + 5)
             return 0;
-
         int left = 0;
         int right = 1;
         int vowels = this->isVowel(word[left]);
         long long substrings = 0;
         unordered_map<int, int> vowelCount;
-
-        if (this->isVowel(word[left]))
+        if (this->isVowel(word[left])) 
             vowelCount[word[left]] = 1;
-
-        while (right < word.size()) {
+            
+        while(right < word.size()) {
             if (this->isVowel(word[right])) {
                 vowels++;
                 vowelCount[word[right]]++;
             }
-
             int consonants = right + 1 - left - vowels;
+            
+            for(int i = left; i <= right; i++) {
+                cout<<word[i];
+            }
+            cout<<endl;
+            /*
+            cout<<endl;
             cout << "Left: " << left << " , right: " << right << endl;
-            cout << "Vowels: " << vowels << " , consonants: " << consonants
-                 << endl
-                 << endl;
-
+            cout << "Vowels: " << vowels << " , consonants: " << consonants<<endl;
+            cout<<"substrings: "<<substrings<<endl<< endl;
+            */
+                 
             if (vowelCount.size() < 5 || consonants < k) {
                 right++;
                 continue;
             }
-
+            
             if (consonants == k) {
                 substrings++;
+                right++;
+                continue;
             }
-
+            
             if (consonants > k) {
-                int consonantsTaken = 0;
-                while (consonantsTaken < consonants - k && left < right) {
-                    if (this->isVowel(word[left])) {
-                        if(vowelCount[word[left]] > 1) {
-                            vowelCount[word[left]]--;
-                        } else {
-                           vowelCount.erase(word[left]);
-                        }
-                        vowels--;
+                if((!this->isVowel(word[right]) && this->isVowel(word[left]))) {
+                    right--;
+                } else if(this->isVowel(word[right])){
+                    vowels--;
+                    if(vowelCount[word[right]] > 1) {
+                        vowelCount[word[right]]--;
                     } else {
-                        consonantsTaken++;
-                    }
-                    left++;
-                }
-                if (left == right) {
-                    if (this->isVowel(word[left])) {
-                        vowelCount[word[left]] = 1;
-                        vowels = 1;
+                        vowelCount.erase(word[right]);
                     }
                 }
-
-                consonants = right + 1 - left - vowels;
-                if (vowelCount.size() == 5 && consonants == k) {
-                    substrings++;
+                
+                if(vowelCount[word[left]] > 1) {
+                    vowelCount[word[left]]--;
+                } else {
+                    vowelCount.erase(word[left]);
                 }
+                vowels -= this->isVowel(word[left]);
+                left++;
             }
-
-            right++;
         }
-
-        right = word.size() - 1;
-        vowels -= this->isVowel(word[left]);
-        if(vowelCount[word[left]] > 1) {
-            vowelCount[word[left]]--;
-        } else {
-            vowelCount.erase(word[left]);
-        }
-        left++;
-        while(word.size() - left >= k + 5) {
-            int consonants = right + 1 - left - vowels;
-            if (vowelCount.size() == 5 && consonants == k) {
-                substrings++;
-            }
-
-            if (this->isVowel(word[left])) {
-                vowels--;
-                vowelCount.erase(word[left]);
-            }
-            left++;
-        }
-
+       
         return substrings;
     }
 };
