1c8559d1ccf0f178278291eb0e4ee45309926604
Simon Giraldo <simongiraldoc@gmail.com>
2025-03-10 15:50:00 -0500

    improved exercise 3306 on c++

--- c++/medium/3306.substrings_conatining_every_vowel_k_consonants_II/count_substring_with_every_vowel_and_k_consonants_II.cpp
+++ c++/medium/3306.substrings_conatining_every_vowel_k_consonants_II/count_substring_with_every_vowel_and_k_consonants_II.cpp
@@ -24,32 +24,36 @@ public:
         int right = 1;
         int vowels = this->isVowel(word[left]);
         long long substrings = 0;
-        unordered_map<int, int> vowelCount;
+        unordered_map<char, int> vowelCount;
+        
         if (this->isVowel(word[left])) 
             vowelCount[word[left]] = 1;
             
         while(left < word.size()+1 - (k+5)) {
             char letter = word[right];
-            if(right+1 == word.size()) {
+            if(right > word.size()-1) {
                 letter = word[left];
-            }
-            
-            if (this->isVowel(letter)) {
+                right = word.size()-1;
+            } else {
+                if (this->isVowel(letter)) {
                 vowels++;
                 vowelCount[letter]++;
             }
+            }
             
             int consonants = right + 1 - left - vowels;
-            
+            /*
             for(int i = left; i <= right; i++) {
                 cout<<word[i];
             }
             cout<<endl;
-            /*
+            
             cout << "Left: " << left << " , right: " << right << endl;
             cout << "Vowels: " << vowels << " , consonants: " << consonants<<endl;
-            cout<<"substrings: "<<substrings<<endl<< endl;
+            cout<<"substrings: "<<substrings<<endl;
+            cout<<"vowelCount: "<<vowelCount.size()<<endl<<endl;
             */
+            
                  
             if (vowelCount.size() < 5 || consonants < k) {
                 if(right+1 == word.size()) {
@@ -68,17 +72,17 @@ public:
             
             if (consonants == k) {
                 substrings++;
-                if(right+1 == word.size()) {
+                if(right == word.size()-1) {
                     if(vowelCount[word[left]] > 1) {
                         vowelCount[word[left]]--;
                     } else {
                         vowelCount.erase(word[left]);
                     }
                     vowels -= this->isVowel(word[left]);
+                    //cout<<"New vowels mov Left: "<<vowels<<endl<<endl;
                     left++;
-                } else {
-                    right++;
                 }
+                right++;
                 continue;
             }
             
@@ -111,10 +115,36 @@ public:
 int main()
 {   
     Solution* sn = new Solution();
-    
-    string case3 = "iqeaouqi"; // k=2, result = 3
-    long long result3 = sn->countOfSubstrings(case3, 2);
+    /*
+    string case3 = "ieaouqqieaouqq"; // k=1, result = 3
+    long long result3 = sn->countOfSubstrings(case3, 1);
     cout<<"Result for "<<case3<<": "<<result3<<endl;
+    */
+    string case0 = "iqeaouqi"; // k=2, result = 3
+    long long test0 = sn->countOfSubstrings(case0, 2);
+    string result0 = test0 == 3 ? "PASS" : "FAIL";
+    cout<<"Result for "<<case0<<": "<<test0<<"  "<<result0<<endl;
+    
+    string case1 = "aoaiuefi";
+    long long test1 = sn->countOfSubstrings(case1, 1);
+    string result1 = test1 == 4 ? "PASS" : "FAIL";
+    cout<<"Result for "<<case1<<": "<<test1<<"  "<<result1<<endl;
+    
+    string case2 = "aeioqq";
+    long long test2 = sn->countOfSubstrings(case2, 1);
+    string result2 = test2 == 0 ? "PASS" : "FAIL";
+    cout<<"Result for "<<case2<<": "<<test2<<"  "<<result2<<endl;
+    
+    string case3 = "aeiou";
+    long long test3 = sn->countOfSubstrings(case3, 0);
+    string result3 = test3 == 1 ? "PASS" : "FAIL";
+    cout<<"Result for "<<case3<<": "<<test3<<"  "<<result3<<endl;
+    
+    string case4 = "ieaouqqieaouqq";
+    long long test4 = sn->countOfSubstrings(case4, 1);
+    string result4 = test4 == 3 ? "PASS" : "FAIL";
+    cout<<"Result for "<<case4<<": "<<test4<<"  "<<result4<<endl;
+
 
     return 0;
 }
