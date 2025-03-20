9c6d259a4d592755c378c862afae4d021234bb03
Simon Giraldo <simongiraldoc@gmail.com>
2025-03-20 15:33:42 -0500

    exercise 5 on c++, first version

---
+++ c++/medium/5.longest_palindromic_substring/longest_palindromic_substring.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.size() == 1) return s;
+        if(s.size() == 2) {
+            return s[0] == s[1] ? s : string(1, s[0]);
+        }
+
+        string longest = "";
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int left = 0;
+        int right = s.size()-1;
+        bool isPalindrome = false;
+
+        while(left < right) {
+            if(s[left] == s[right]) {
+                if(!isPalindrome) {
+                    leftIndex = left;
+                    rightIndex = right;
+                    isPalindrome = true;
+                }
+            } else {
+                leftIndex = 0;
+                rightIndex = 0;
+                isPalindrome = false;
+            }
+
+            if(isPalindrome) {
+                left++;
+                right--;
+                continue;
+            }
+            
+            if(s[left+1] == s[right] && s[right-1] != s[left]) {
+                left++;
+            }
+            else if(s[right-1] == s[left] && s[left+1] != s[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        for(int i = leftIndex; i <= rightIndex; i++) {
+            longest += s[i];
+        }
+
+        return longest;
+    }
+};
