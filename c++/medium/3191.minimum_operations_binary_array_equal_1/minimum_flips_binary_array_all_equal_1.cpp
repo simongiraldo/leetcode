ce828216adc1ad129e850da515a1cb70b2bcc316
Simon Giraldo <simongiraldoc@gmail.com>
2025-03-20 13:27:39 -0500

    exercise 3191 on c++ added

---
+++ c++/medium/3191.minimum_operations_binary_array_equal_1/minimum_flips_binary_array_all_equal_1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int flipCount = 0;
+        int onesCount = 0;
+
+        for(int i = 0; i < nums.size()-2; i++) {
+            int left = nums[i];
+            int middle = nums[i+1];
+            int right = nums[i+2];
+
+            onesCount++;
+
+            if(left == 1) {
+                continue;
+            }
+
+            nums[i] = left^1;
+            nums[i+1] = middle^1;
+            nums[i+2] = right^1;
+            flipCount++;
+        }
+
+        onesCount += nums[nums.size()-2];
+        onesCount += nums[nums.size()-1];
+
+        return onesCount == nums.size() ? flipCount : -1;
+    }
+};
